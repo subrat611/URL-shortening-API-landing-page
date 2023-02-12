c757450dc3c0ebda28a3e916bcc2321fa4738bbe
subrat <subratj611@gmail.com>
2023-02-12 20:08:48 +0530

    bg-boost section done

---
+++ src/components/BgBoostSection/index.jsx
@@ -0,0 +1,10 @@
+import "./bgboost.scss";
+
+export default function BgBoostSection() {
+  return (
+    <div className="bg-boost-wrapper">
+      <p className="bg-boost-title">Boost your links today</p>
+      <button className="bg-boost-action-btn">Get Started</button>
+    </div>
+  );
+}
