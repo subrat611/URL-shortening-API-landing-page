d5aa158cc058da6f72553fbf75e32a05db9448f6
subrat <subratj611@gmail.com>
2023-02-12 11:11:08 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import URLShort from "./components/URLShort";
+import StatisticSection from "./components/StatisticSection";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HeroSection />
       <div className="o-app-hero-wrapper">
-        <HeroSection />
         <URLShort />
+        {/* generated url show here */}
+        <StatisticSection />
       </div>
     </div>
   );
