d1d462e68c6f0b8159cd9e02c5d38968dac467c6
subrat <subratj611@gmail.com>
2023-02-13 13:19:19 +0530

    validation and style update

--- src/components/URLShort/index.jsx
+++ src/components/URLShort/index.jsx
@@ -1,15 +1,52 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./urlshort.scss";
 
 export default function URLShort() {
+  const [inputUrl, setInputUrl] = useState("");
+  const [inputError, setInputError] = useState(false);
+
+  useEffect(() => {
+    getShortUrl();
+  }, []);
+
+  async function getShortUrl() {
+    if (inputUrl !== "") {
+      const res = await (
+        await fetch(`https://api.shrtco.de/v2/shorten?url=${inputUrl}`)
+      ).json();
+    }
+  }
+
+  const validateURL = () => {
+    if (inputUrl !== "") {
+      getShortUrl();
+      setInputError(false);
+    } else {
+      setInputError(true);
+    }
+  };
+
   return (
-    <form className="url-form-wrapper">
+    <form
+      className="url-form-wrapper"
+      onSubmit={(e) => {
+        e.preventDefault();
+        validateURL();
+      }}
+    >
       <div className="url-input-wrapper">
         <input
           type="text"
-          className="url-input"
+          className={inputError ? "url-input input-error" : "url-input"}
           placeholder="Shorten a link here..."
+          onChange={(e) => setInputUrl(e.target.value)}
         />
-        <span className="url-input-error">Please add a link</span>
+        <span
+          className={inputError ? "url-input-error visible" : "url-input-error"}
+        >
+          Please add a link
+        </span>
       </div>
       <button type="submit" className="form-submit-btn">
         Shorten It!
